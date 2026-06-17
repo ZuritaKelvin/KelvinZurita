5a4f184bc8ed501ba5d2f3aa03a22a6973049ee6
Kelvin Zurita <kelvinzurita2015@gmail.com>
2026-06-17 14:08:50 +0200

    fix: astro adapter added for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+
   vite: {
     ssr: {
       noExternal: ["@fontsource-variable/onest"],
     },
   },
+
+  adapter: vercel(),
 });
